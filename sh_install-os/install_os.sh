f93ef03a4f46cf6dc49a5504f805b44d57ec0cb0
Miguel Rivas <github.5yeil@simplelogin.fr>
2025-11-14 07:07:32 -0800

    updating os instructions

--- sh_install-os/install_os.sh
+++ sh_install-os/install_os.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# environment: Cinnamon
-# version: 21
+# environment: LMDE
+# version: 6
 
 echo "Installing dependencies..."
 sudo apt update && sudo apt -y install \
@@ -10,18 +10,6 @@ git \
 xclip \
 xsel \
 wget \
-snapd
-
-echo "NVM / part 1"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-
-echo "Yarn / part 1"
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
-echo "VScode"
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 
 sudo apt update && sudo apt -y install \
 vlc \
@@ -29,40 +17,21 @@ winff \
 ffmpeg \
 inkscape \
 gimp \
-trimage \
-android-tools-adb \
-android-tools-fastboot \
-arc-theme \
-papirus-icon-theme \
-breeze-cursor-theme \
 imagemagick \
+nvim \
+blender \
+tree \
+alsamixer \
+lazygit \
+obs \
+screenkey \
 gparted \
-docker* \
-python3-tk \
-libimage-exiftool-perl \
-yarn \
-code
+bc \
+ncdu \
 
-echo "Reset terminal to start NVM..."
-exec bash -l
 
-echo "NVM / part 2"
-nvm install --lts
-
-echo "Docker / part 2 / Adding docker persmissions for current user"
-sudo usermod -aG docker ${USER}
-
-echo "NVM / part 3 / Install global node packages"
-npm i -g @vue/cli
-npm i -g typescript
-
-echo "SSH key generation"
-ssh-keygen -t ed25519
-
-echo "
-Get the following packages from Software Manager:
-- blender-flatpak
-- unity-hub
-- git kraken
-- video downloader flatpack
-"
+look in the web for the folloing:
+yazi \
+s-tui \
+ghostty \
+resterm \
